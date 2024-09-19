196556c5baef9de1b446792a5db4c77a1b4b95b2
Mak Mach <471842@ntti.edu.kh>
2024-09-19 17:08:18 +0700

    #21 local images

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,10 +1,7 @@
-
-import React from 'react'
+import myPic from "../assets/pic.png";
 
 const Image = () => {
-  return (
-    <img src="https://images.pexels.com/photos/28186197/pexels-photo-28186197/free-photo-of-a-woman-with-an-umbrella-in-black-and-white.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="my-picture" />
-  )
+  return <img src={myPic} height={300} alt="my-picture" />
 }
 
 export default Image
